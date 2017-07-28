22b5f9117f8cc772d4316252993dfbaf85606b85
Alexander Stride <alexanderstride@alexandridesmbp.home>
2017-07-28 20:00:52 +0100

    Unit 11 completed

--- js/main.js
+++ js/main.js
@@ -24,17 +24,29 @@ var checkForMatch = function() {
 	}
 }
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+	var cardId = this.getAttribute("data-id");
 	var currCard = cards[cardId];
 	console.log("User flipped " + currCard.rank)
 	console.log(currCard.suit);
 	console.log(currCard.cardImage)
 	cardsInPlay.push(currCard.rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for(var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+
+	}
+}
+
+createBoard();
